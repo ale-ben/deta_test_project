d2459f63a0e574eb85328ad0eb6f0f7d917e7e37
Alessandro Benetton <aleben98@gmail.com>
2023-11-24 16:21:47 +0100

    [master] stable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,26 @@
-import './globals.css'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Providers } from './providers';
+
+export const metadata: Metadata = {
+	title: 'Fuel Logger',
+	description: 'Tool for tracking fuel consumption of a vehicle'
+};
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html suppressHydrationWarning lang="en" className="dark">
+			<body className="flex h-screen w-screen flex-col">
+				<Providers>
+					<div className="h-full w-full overflow-hidden">
+						{children}
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
-       Hello World.
-	   <p>I am a test page</p>
-      </div>
-    </main>
-  )
+export default async function Dashboard() {
+	return <div className="mt-2 text-center text-2xl">Fuel Logs</div>;
 }
